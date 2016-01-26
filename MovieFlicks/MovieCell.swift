2f58d31210f284f343eab688e8ca355b85fc8308
Aditya Purandare <aditya.p1993@hotmail.com>
2016-01-25 23:05:38 -0600

    Add layout for Poster View

---
+++ MovieFlicks/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  MovieFlicks
+//
+//  Created by Aditya Purandare on 25/01/16.
+//  Copyright © 2016 Aditya Purandare. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- MovieFlicks/MoviesViewController.swift
+++ MovieFlicks/MoviesViewController.swift
@@ -68,13 +68,15 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
         
         let movie = movies![indexPath.row]
         let title = movie["title"] as! String
+        let overview = movie["overview"] as! String
         
+        cell.titleLabel.text = title
+        cell.overviewLabel.text = overview
         
-        cell.textLabel!.text = title
         //print("row \(indexPath.row)")
         return cell
     }
