b48090f5bac1eceea9bab9427182b2fc9a240aff
Aditya Purandare <aditya.p1993@hotmail.com>
2016-02-01 18:39:14 -0600

    Low image to high image built

--- MovieFlicks/MovieDetailsViewController.swift
+++ MovieFlicks/MovieDetailsViewController.swift
@@ -42,12 +42,53 @@ class MovieDetailsViewController: UIViewController {
         overviewLabel.sizeToFit()
         
         if let posterPath = movie["poster_path"] as? String {
-            let posterUrl = NSURL(string: baseUrl + posterPath)
-            backdropView.setImageWithURL(posterUrl!)
+            
+            //let posterUrl = NSURL(string: baseUrl + posterPath)
+            //backdropView.setImageWithURL(posterUrl!)
+            let smallBaseURL = "http://image.tmdb.org/t/p/w154"
+            let largeBaseURL = "http://image.tmdb.org/t/p/w500"
+            
+            let smallImageRequest = NSURLRequest(URL: NSURL(string: smallBaseURL + posterPath)!)
+            let largeImageRequest = NSURLRequest(URL: NSURL(string: largeBaseURL + posterPath)!)
+            
+            self.backdropView.setImageWithURLRequest(
+                smallImageRequest,
+                placeholderImage: nil,
+                success: { (smallImageRequest, smallImageResponse, smallImage) -> Void in
+                    
+                    // smallImageResponse will be nil if the smallImage is already available
+                    // in cache (might want to do something smarter in that case).
+                    self.backdropView.alpha = 0.0
+                    self.backdropView.image = smallImage;
+                    
+                    UIView.animateWithDuration(0.3, animations: { () -> Void in
+                        
+                        self.backdropView.alpha = 1.0
+                        
+                        }, completion: { (sucess) -> Void in
+                            
+                            // The AFNetworking ImageView Category only allows one request to be sent at a time
+                            // per ImageView. This code must be in the completion block.
+                            self.backdropView.setImageWithURLRequest(
+                                largeImageRequest,
+                                placeholderImage: smallImage,
+                                success: { (largeImageRequest, largeImageResponse, largeImage) -> Void in
+                                    
+                                    self.backdropView.image = largeImage;
+                                    
+                                },
+                                failure: { (request, response, error) -> Void in
+                                    // do something for the failure condition of the large image request
+                                    // possibly setting the ImageView's image to a default image
+                            })
+                    })
+                },
+                failure: { (request, response, error) -> Void in
+                    // do something for the failure condition
+                    // possibly try to get the large image
+            })
         }
         else {
-            // No poster image. Can either set to nil (no image) or a default movie poster image
-            // that you include as an asset
             backdropView.image = nil
         }
         
